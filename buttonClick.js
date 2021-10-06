d0e3b448af9cd6172e3e2a860fe14070465fef8e
Finn Wohner <71097713+frriNz@users.noreply.github.com>
2021-10-06 17:42:31 +0100

    Update buttonClick.js

--- buttonClick.js
+++ buttonClick.js
@@ -2,5 +2,6 @@ const btn = document.querySelector('button');
 const img = document.querySelector('img');
 
 btn.addEventListener('click', () => {
+  btn.remove();
   img.style.visibility = 'visible';
 });
